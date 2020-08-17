738885fcce0800515df00a0b2e340b5a3a0f3b81
daumhch <daumhch@naver.com>
2020-08-17 23:22:43 +0900

    gson 넘기는 것 까지 확인

--- src/main/java/hcbae/demo/index/IndexController.java
+++ src/main/java/hcbae/demo/index/IndexController.java
@@ -1,5 +1,7 @@
 package hcbae.demo.index;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
@@ -7,9 +9,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.gson.Gson;
+
+import hcbae.demo.domain.Address;
 import hcbae.demo.service.AddressService;
+import lombok.extern.log4j.Log4j;
 
-
+@Log4j
 @Controller
 public class IndexController {
 	
@@ -18,7 +24,14 @@ public class IndexController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Model model) {
-		addressService.readAll();
+		List<Address> list = addressService.readAll();
+		for(Address a : list) {
+			log.info("###:"+a);
+		}
+		
+		Gson gList = new Gson();
+		String gsonList = gList.toJson(list);
+		model.addAttribute("list", gsonList);
 		return "index";
 	}
 	
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -10,5 +10,8 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+
+<P>  list : ${list} </P>
+
 </body>
 </html>
